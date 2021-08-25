40776359242ed6c20b0cc95fd1c47cf5290d81cb
Artem <artmurashko@yandex.ru>
2021-08-25 17:28:47 +0300

    Fix some bugs

--- week02/ex4.c
+++ week02/ex4.c
@@ -8,10 +8,18 @@ void swapTwoNumbers(int *a, int *b) {
 
 int main() {
     int a = 0, b = 0;
+
     printf("Enter a 1st number: ");
-    scanf("%d", &a);
-    printf("Enter a 1st number: ");
-    scanf("%d", &b);
+    if (scanf("%d", &a) != 1) {
+        printf("You've entered not a number");
+        return 0;
+    }
+
+    printf("Enter a 2nd number: ");
+    if (scanf("%d", &b) != 1) {
+        printf("You've entered not a number");
+        return 0;
+    }
 
     printf("Before swap: a = %d, b = %d\n", a, b);
     swapTwoNumbers(&a, &b);
